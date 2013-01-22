33549defd0b3dce2827ac048d9ba0f320c4c970c
modilabs-bumblebee <modilabs.bumblebee@gmail.com>
2013-01-22 13:45:13 +0300

    Added formhub dashboard and submissions job

---
+++ jobs/submissions.rb
@@ -0,0 +1,15 @@
+require "./lib/graphite"
+
+# last started parkingsessions
+SCHEDULER.every '10s', :first_in => 0 do
+    # Create an instance of our helper class
+    q = Graphite.new "http://graphite.formhub.org/"
+    
+    stat_name = "summarize(stats_counts.response.200, \"1day\")"
+    
+    # get points for the last half hour
+    points = q.points stat_name, "-7days"
+    
+    # send to dashboard, so the number the meter and the graph widget can understand it
+    send_event 'submissions', { points: points }
+end
