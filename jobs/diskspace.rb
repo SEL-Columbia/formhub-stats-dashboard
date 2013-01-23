e5e87467837e9b36ba47177fb9852ade1abe666a
modilabs-bumblebee <modilabs.bumblebee@gmail.com>
2013-01-23 15:41:02 +0300

    Added widgets for daily and hourly submissions and disk usage

---
+++ jobs/diskspace.rb
@@ -0,0 +1,16 @@
+require "./lib/graphite"
+
+# last started parkingsessions
+SCHEDULER.every '10s', :first_in => 0 do
+    # Create an instance of our helper class
+    q = Graphite.new "http://graphite.formhub.org/"
+    
+    stat_name = "summarize(servers.formhub_dot_org.diskspace.root.byte_avail,\"30min\",\"avg\",true)"
+    
+    # get average for the last half hour
+    space_avail = q.value stat_name, "-30min"
+    space_avail = (space_avail/(1024**2)).round()
+    
+    # send to dashboard, so the number the meter and the graph widget can understand it
+    send_event 'diskspace', { value: space_avail }
+end
